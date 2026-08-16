104d329b017858ca02d1d29f6b447f451f5475ad
KabilanMadurai <kabiltamil1330@gmail.com>
2026-08-17 00:05:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/Favourite-Student-List/",
+  base: '/Favourite-Student-List/',
   plugins: [react()],
 })
